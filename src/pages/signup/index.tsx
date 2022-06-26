8731b02d94679c8bc16b524139b1f7224941ae0d
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-06-26 17:22:08 +0900

    signupページの修正

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,8 +1,27 @@
-import Link from "next/link";
 import { GithubAuthProvider, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { FC, ReactNode } from "react";
 import { GoogleIcon } from "src/components/ui-libraries/GoogleIcon";
 import { auth } from "src/components/utils/libs/firebase";
+import { GitHubIcon } from "src/components/ui-libraries/GithubIcon";
 import type { NextPage } from "next";
+
+type ButtonProps = {
+  Icon: ReactNode;
+  text: string;
+  onClick: () => void;
+};
+
+const LoginButton: FC<ButtonProps> = ({ Icon, text, onClick }) => {
+  return (
+    <div
+      className="flex gap-3 justify-center items-center py-2 px-4 mb-8 w-72 bg-gray-50  hover:bg-gray-100 rounded-2xl border border-gray-300 hover:shadow-md cursor-pointer"
+      onClick={onClick}
+    >
+      {Icon}
+      <p className="text-base font-bold">{`Login with ${text}`}</p>
+    </div>
+  );
+};
 
 const signUp: NextPage = () => {
   const googleProvider = new GoogleAuthProvider();
@@ -29,56 +48,11 @@ const signUp: NextPage = () => {
   };
 
   return (
-    <div className="h-screen bg-white">
-      <div className="flex flex-wrap w-full">
-        <div className="flex flex-col w-full md:w-1/2">
-          <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:px-24 md:pt-0 lg:px-32">
-            <p className="text-3xl text-center">Welcome.</p>
-          </div>
-        </div>
-        <div className="flex flex-col w-full md:w-1/2">
-          {/* 入会方法側・ログイン */}
-          <div className="flex flex-col  px-8 pt-8 my-auto md:px-24 md:pt-0 lg:px-32">
-            <p className="mt-28 text-3xl font-bold text-center">入会方法</p>
-            <div className="flex flex-row">
-              <div className="flex justify-center items-center mt-10 w-8 h-8 bg-gray-700 rounded-full">
-                <p className="text-xl text-white">1</p>
-              </div>
-              <div className="flex-col">
-                <p className="mt-10 ml-6 text-xl">下記よりアカウント作成</p>
-                <p className="mt-2 ml-6 text-xs">Google連携が必要です</p>
-              </div>
-            </div>
-            <div className=" flex flex-row">
-              <div className="flex justify-center items-center mt-10 w-8 h-8 bg-gray-700 rounded-full">
-                <p className="text-xl text-white">2</p>
-              </div>
-              <div className="flex-col">
-                <p className="mt-10 ml-6 text-xl">プロフィールの記入</p>
-                <p className="mt-2 ml-6 text-xs">あとで変更することも可能です</p>
-              </div>
-            </div>
-            <button onClick={signInWithGoogle}>
-              <div className="flex justify-center items-center">
-                <GoogleIcon />
-                <span>Sign up with Google</span>
-              </div>
-            </button>
-            <button onClick={signInWithGitHub}>
-              <div className="flex justify-center items-center">
-                <span>github</span>
-              </div>
-            </button>
-            <div className=" pt-6 pb-12">
-              <p className="text-sm">
-                既にアカウントをお持ちですか？
-                <Link href="/signin">
-                  <a className="text-blue-400">サインイン</a>
-                </Link>
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <h1 className="pb-10 text-2xl font-bold text-center">ログインしてください</h1>
+        <LoginButton Icon={<GoogleIcon />} text="Google" onClick={signInWithGoogle} />
+        <LoginButton Icon={<GitHubIcon />} text="GitHub" onClick={signInWithGitHub} />
       </div>
     </div>
   );
@@ -87,4 +61,3 @@ const signUp: NextPage = () => {
 export default signUp;
 
 // TODO: 不要なconsoleを消す
-// TODO: スタイルの修正
