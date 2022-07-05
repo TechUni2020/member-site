a5f3cd60fa63f52fa34745fcc661c200d2a1c130
code-yy <jiyeyouren523@gmail.com>
2022-07-06 03:24:31 +0900

    ♻️ firestoreに保存 → 取得までできるように

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
-import { RecoilRoot } from "recoil";
-import type { AppProps } from "next/app";
+import { RecoilRoot, useRecoilState } from "recoil";
 import "src/styles/globals.css";
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { User } from "firebase/auth";
+import { db } from "src/components/utils/libs/firebase";
+import { currentUserState } from "src/global-states/atoms";
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
+      {router.pathname === "/signup" ? null : <Auth />}
     </RecoilRoot>
   );
 };
 export default App;
 
-// todo: ログイン済みのユーザー情報があるかどうかをチェックする
+export const Auth = () => {
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+
+  useEffect(() => {
+    const uid: string | null = localStorage.getItem("currentUser");
+    const docRef = doc(db, "users", uid ?? "");
+    getDoc(docRef).then((doc) => {
+      if (doc.exists()) {
+        setCurrentUser(doc.data() as User);
+      }
+    });
+  }, []);
+
+  return null;
+};
--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,13 +1,23 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import { useRecoilState } from "recoil";
 import { Layout } from "src/components/layout";
 import { AppLoading } from "src/components/ui-libraries/AppLoading";
+import { currentUserState } from "src/global-states/atoms";
 
 const Top = () => {
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+
+  useEffect(() => {
+    console.log("currentUser", currentUser);
+  }, [currentUser]);
+
   return (
     <>
       <Layout>
         <Suspense fallback={<AppLoading />}>
           <h1>top</h1>
+          <div>{currentUser?.displayName}</div>
+          <div>{currentUser?.email}</div>
         </Suspense>
       </Layout>
     </>
