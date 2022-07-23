7caab4d93d3967ee826bca6ff0996985fdc6f6b8
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-07-23 15:02:49 +0900

    メンバー

--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -1,18 +1,37 @@
-import { MemberCard } from "src/components/feature/Card";
+import { AdminCard, InterestMember, MemberCard } from "src/components/feature/Card";
 import { Layout } from "src/components/layout";
 
 const Member = () => {
   return (
     <Layout>
+      <div className="flex flex-col flex-wrap gap-5 w-full">
+        <div className="pt-5">
+          <h1 className="font-bold">コミッティー</h1>
+          <div className="flex gap-x-5">
+            <AdminCard />
+            <AdminCard />
+          </div>
+        </div>
+        <div>
+          <h1 className="font-bold">アクティブメンバー</h1>
+          <MemberCard />
+        </div>
+        <div>
+          <h1 className="font-bold">自分の興味のある分野を専門としているメンバー</h1>
+          <InterestMember />
+        </div>
+        <div>
+          <h1 className="font-bold">2回生</h1>
+          <InterestMember />
+        </div>
+        <div className="font-bold text-center">その他</div>
+      </div>
       <h1>Member</h1>
       <p>上に検索バー・分野・学年で絞れるようにする</p>
       <p>コミッティーメンバー</p>
       <p>アクティブメンバー</p>
       <p>自分の興味のある分野を専門としているメンバー一覧</p>
       <p>それぞれ閉じるボタンをつけれるようにする。</p>
-      <div className="flex flex-wrap w-full">
-        <MemberCard />
-      </div>
     </Layout>
   );
 };
